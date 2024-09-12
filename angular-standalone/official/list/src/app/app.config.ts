5efb139c23026eba13f36185ac458c5c1a12b55f
Jim <38460550+jjmhalew@users.noreply.github.com>
2024-09-12 22:46:51 +0200

    chore() add app config file and reference it

---
+++ angular-standalone/official/list/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes, withPreloading(PreloadAllModules))
+  ]
+};
